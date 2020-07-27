011d51a2ddb1c6d75cb9257d8d096c3dff57f09a
Michal Mrozek <michal.mrozek@intel.com>
2020-07-27 15:05:59 +0200

    pass proper size.

--- source/tests/ocl/new_resources_submission_host_ocl.cpp
+++ source/tests/ocl/new_resources_submission_host_ocl.cpp
@@ -39,7 +39,7 @@ static bool run(const NewResourcesSubmissionHostArguments &arguments, Statistics
     // Benchmark
     for (int i = 0; i < arguments.iterations; i++) {
         timer.measureStart();
-        int *hostMemory = (int *)clHostMemAllocINTEL(opencl.context, nullptr, 64, 0, &retVal);
+        int *hostMemory = (int *)clHostMemAllocINTEL(opencl.context, nullptr, sizeInBytes, 0, &retVal);
         ASSERT_CL_SUCCESS(clSetKernelArgSVMPointer(kernel, 0, hostMemory));
         retVal |= clEnqueueNDRangeKernel(opencl.commandQueue, kernel, 1, nullptr, &gws, nullptr, 0, nullptr, nullptr);
         retVal |= clFinish(opencl.commandQueue);
