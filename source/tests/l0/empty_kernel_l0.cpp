a7e202f558b63051b4e7561383f1524c34cc0b04
Maciej Dziuban <maciej.dziuban@intel.com>
2020-07-27 11:57:38 +0200

    Delete unused code

--- source/tests/l0/empty_kernel_l0.cpp
+++ source/tests/l0/empty_kernel_l0.cpp
@@ -65,45 +65,6 @@ static bool run(const EmptyKernelArguments &arguments, Statistics &statistics) {
     ASSERT_ZE_RESULT_SUCCESS(zeModuleDestroy(module));
     ASSERT_ZE_RESULT_SUCCESS(zeCommandListDestroy(cmdList));
     return true;
-#if 0
-    Opencl opencl;
-    Timer timer;
-    cl_int retVal;
-    const auto gws = arguments.workgroupCount * arguments.workgroupSize;
-    const auto lws = arguments.workgroupSize;
-
-    // Create kernel
-    const char *source = "__kernel void empty() {}";
-    const auto sourceLength = strlen(source);
-    cl_program program = clCreateProgramWithSource(opencl.context, 1, &source, &sourceLength, &retVal);
-    ASSERT_CL_SUCCESS(retVal);
-    retVal = clBuildProgram(program, 1, &opencl.device, nullptr, nullptr, nullptr);
-    ASSERT_CL_SUCCESS(retVal);
-    cl_kernel kernel = clCreateKernel(program, "empty", &retVal);
-    ASSERT_CL_SUCCESS(retVal);
-
-    // Warmup, kernel
-    size_t warmupOffset = 8;
-    retVal |= clEnqueueNDRangeKernel(opencl.commandQueue, kernel, 1, &warmupOffset, &gws, &lws, 0, nullptr, nullptr);
-    retVal |= clFinish(opencl.commandQueue);
-    ASSERT_CL_SUCCESS(retVal);
-
-    // Benchmark
-    for (int i = 0; i < arguments.iterations; i++) {
-        // Enqueue empty kernel and measure it
-        timer.measureStart();
-        retVal |= clEnqueueNDRangeKernel(opencl.commandQueue, kernel, 1, nullptr, &gws, &lws, 0, nullptr, nullptr);
-        retVal |= clFinish(opencl.commandQueue);
-        timer.measureEnd();
-        ASSERT_CL_SUCCESS(retVal);
-        statistics.pushValue(timer.Get());
-    }
-
-    // Cleanup
-    ASSERT_CL_SUCCESS(clReleaseKernel(kernel));
-    ASSERT_CL_SUCCESS(clReleaseProgram(program));
-    return true;
-#endif
 }
 
 static RegisterTestCase<EmptyKernel> registerTestCase(run, Api::L0);
