48b32fe50a78214800f40fcf45a5de3dabcbe603
Maciej Dziuban <maciej.dziuban@intel.com>
2020-08-10 22:01:40 +0200

    Refactor CMake options

--- CMakeOptions.cmake
+++ CMakeOptions.cmake
@@ -1,21 +1,11 @@
-# Option for output dir
-if (NOT OUTPUT_DIR)
-    set(OUTPUT_DIR "${CMAKE_BINARY_DIR}/bin")
-endif()
-message(STATUS "Binary output directory: ${OUTPUT_DIR}")
+macro(benchmark_option NAME DEFAULT_VALUE)
+    if (NOT DEFINED ${NAME})
+        set(${NAME} ${DEFAULT_VALUE})
+    endif()
+    message(STATUS "  ${NAME}=${${NAME}}")
+endmacro()
 
-# Option for OCL
-option(TEST_OCL "Enable OpenCL benchmarks" ON)
-if(NOT TEST_OCL)
-    message(STATUS "Skipping OpenCL tests")
-else()
-    message(STATUS "Including OpenCL tests")
-endif()
-
-# Option for L0
-option(TEST_L0 "Enable LevelZero benchmarks" ON)
-if(NOT TEST_L0)
-    message(STATUS "Skipping LevelZero tests")
-else()
-    message(STATUS "Including LevelZero tests")
-endif()
+message(STATUS "Build options:")
+benchmark_option(OUTPUT_DIR "${CMAKE_BINARY_DIR}/bin")
+benchmark_option(TEST_OCL ON)
+benchmark_option(TEST_L0 ON)
