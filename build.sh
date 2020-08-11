56f6c1e4624a6c39a9f15e7f8e7aae23f425d0cc
Maciej Dziuban <maciej.dziuban@intel.com>
2020-08-11 14:20:11 +0200

    Set proxy in build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,8 @@
+git config --global http.proxy http://proxy-chain.intel.com:911
+git config --global https.proxy http://proxy-chain.intel.com:912
+export http_proxy="http://proxy-chain.intel.com:911"
+export https_proxy="htts://proxy-chain.intel.com:912"
+
 mkdir build -p
 cd build
 cmake .. $@
