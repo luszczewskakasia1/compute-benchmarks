1cb668d3473fc1d786accb554768cfbb7b1cef21
Maciej Dziuban <maciej.dziuban@intel.com>
2020-11-03 12:40:24 +0100

    Multitile refactor 2/n - add MultiDeviceSelectionTestCaseArgument

---
+++ source/framework/test_case_argument/test_case_argument_multi_device_selection.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include "framework/enum/device_selection.h"
+#include "framework/test_case_argument/test_case_argument_bitfield_enum.h"
+
+struct MultiDeviceSelectionTestCaseArgument : BitfieldEnumTestCaseArgument<MultiDeviceSelectionTestCaseArgument, DeviceSelection> {
+    using BitfieldEnumTestCaseArgument::BitfieldEnumTestCaseArgument;
+    ThisType &operator=(EnumType value) {
+        this->value = value;
+        return *this;
+    }
+
+    const static inline std::string enumName = "multi device selection";
+    const static inline EnumType zeroEnumValue = EnumType::Unknown;
+    const static inline EnumType enumValues[5] = {EnumType::Root, EnumType::Tile0, EnumType::Tile1, EnumType::Tile2, EnumType::Tile3};
+    const static inline std::string enumValuesNames[5] = {"Root", "Tile0", "Tile1", "Tile2", "Tile3"};
+};
