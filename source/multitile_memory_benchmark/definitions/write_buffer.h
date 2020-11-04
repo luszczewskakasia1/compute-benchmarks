410c649805f6fe31820d9ca4ebfa437d74996cb1
Maciej Dziuban <maciej.dziuban@intel.com>
2020-11-04 13:17:09 +0100

    Add multitile WriteBuffer test

---
+++ source/multitile_memory_benchmark/definitions/write_buffer.h
@@ -0,0 +1,43 @@
+#pragma once
+
+#include "framework/test_case/test_case.h"
+#include "framework/test_case_argument/test_case_argument_compression.h"
+#include "framework/test_case_argument/test_case_argument_device_selection.h"
+#include "framework/test_case_argument/test_case_argument_multi_device_selection.h"
+
+#include <sstream>
+
+struct WriteBufferArguments : TestCaseArguments {
+    MultiDeviceSelectionTestCaseArgument contextPlacement;
+    DeviceSelectionTestCaseArgument queuePlacement;
+    DeviceSelectionTestCaseArgument bufferPlacement;
+    ByteSizeTestCaseArgument size;
+    CompressionBooleanTestCaseArgument compressed;
+    BooleanTestCaseArgument useEvents;
+
+    WriteBufferArguments()
+        : contextPlacement(*this, "context", "How context will be created"),
+          queuePlacement(*this, "queue", "Which device within the context will perform the copy"),
+          bufferPlacement(*this, "memory", "Placement of memory for the source buffer"),
+          size(*this, "size", "Size of the buffers"),
+          compressed(*this, "compressed", "Select if source buffer is to be compressed."),
+          useEvents(*this, "useEvents", "Select if GPU-side measurements should be done") {}
+
+    bool validateArgumentsExtra() const override {
+        return DeviceSelectionHelper::isSubset(contextPlacement, queuePlacement) &&
+               DeviceSelectionHelper::isSubset(contextPlacement, bufferPlacement);
+    }
+};
+
+class WriteBuffer : public TestCase<WriteBufferArguments> {
+  public:
+    using TestCase<WriteBufferArguments>::TestCase;
+
+    std::string getHelp() const override {
+        return "allocates an OpenCL buffer and measures write bandwidth.";
+    }
+
+    std::string getTestCaseName() const override {
+        return "WriteBuffer";
+    }
+};
---
+++ source/multitile_memory_benchmark/gtest/write_buffer.cpp
@@ -0,0 +1,43 @@
+#include "multitile_memory_benchmark/definitions/write_buffer.h"
+
+#include "framework/test_case/register_test_case.h"
+#include "framework/utility/common_gtest_args.h"
+
+#include <gtest/gtest.h>
+
+constexpr size_t kiloByte = 1024u;
+constexpr size_t megaByte = 1024u * kiloByte;
+
+static const inline RegisterTestCase<WriteBuffer> registerTestCase{};
+
+class WriteBufferTest : public ::testing::TestWithParam<std::tuple<DeviceSelection, DeviceSelection, DeviceSelection, size_t, bool, bool>> {};
+
+TEST_P(WriteBufferTest, Test) {
+    WriteBufferArguments args;
+    args.api = Api::OpenCL;
+    args.contextPlacement = std::get<0>(GetParam());
+    args.queuePlacement = std::get<1>(GetParam());
+    args.bufferPlacement = std::get<2>(GetParam());
+    args.size = std::get<3>(GetParam());
+    args.compressed = std::get<4>(GetParam());
+    args.useEvents = std::get<5>(GetParam());
+
+    if (!DeviceSelectionHelper::isSubset(args.contextPlacement, args.queuePlacement) ||
+        !DeviceSelectionHelper::isSubset(args.contextPlacement, args.bufferPlacement)) {
+        GTEST_SKIP(); // Placement of all resources must be a subset of context placement
+    }
+
+    WriteBuffer test;
+    test.run(args);
+}
+
+INSTANTIATE_TEST_SUITE_P(
+    WriteBufferTest,
+    WriteBufferTest,
+    ::testing::Combine(
+        ::CommonGtestArgs::contextDeviceSelections(),
+        ::CommonGtestArgs::resourceDeviceSelections(),
+        ::CommonGtestArgs::resourceDeviceSelections(),
+        ::testing::Values(128 * megaByte, 512 * megaByte),
+        ::testing::Values(false, true),
+        ::testing::Values(false)));
