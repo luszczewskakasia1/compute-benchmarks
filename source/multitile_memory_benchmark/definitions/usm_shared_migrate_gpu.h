d2f521cc1b1b7232d5ea9b20824819988d71a900
Maciej Dziuban <maciej.dziuban@intel.com>
2020-11-09 16:08:33 +0100

    Add multitile UsmSharedMigrateGpu test

---
+++ source/multitile_memory_benchmark/definitions/usm_shared_migrate_gpu.h
@@ -0,0 +1,36 @@
+#pragma once
+
+#include "framework/test_case/test_case.h"
+#include "framework/test_case_argument/test_case_argument_compression.h"
+#include "framework/test_case_argument/test_case_argument_multi_device_selection.h"
+#include "framework/test_case_argument/test_case_argument_usm_device_selection.h"
+
+#include <sstream>
+
+struct UsmSharedMigrateGpuArguments : TestCaseArguments {
+    MultiDeviceSelectionTestCaseArgument contextPlacement;
+    UsmSharedDeviceSelectionTestCaseArgument bufferPlacement;
+    ByteSizeTestCaseArgument bufferSize;
+
+    UsmSharedMigrateGpuArguments()
+        : contextPlacement(*this, "context", "How context will be created"),
+          bufferPlacement(*this, "memory", "Placement of memory for the buffer"),
+          bufferSize(*this, "size", "Size of the buffer") {}
+
+    bool validateArgumentsExtra() const override {
+        return DeviceSelectionHelper::isSubset(contextPlacement, DeviceSelectionHelper::withoutHost(bufferPlacement));
+    }
+};
+
+class UsmSharedMigrateGpu : public TestCase<UsmSharedMigrateGpuArguments> {
+  public:
+    using TestCase<UsmSharedMigrateGpuArguments>::TestCase;
+
+    std::string getHelp() const override {
+        return "allocates a unified shared memory buffer and measures time to migrate it from GPU to Gpu.";
+    }
+
+    std::string getTestCaseName() const override {
+        return "UsmSharedMigrateGpu";
+    }
+};
---
+++ source/multitile_memory_benchmark/implementations/ocl/usm_shared_migrate_gpu_ocl.cpp
@@ -0,0 +1,73 @@
+#include "framework/ocl/compression_helper.h"
+#include "framework/ocl/opencl.h"
+#include "framework/ocl/usm_helper.h"
+#include "framework/test_case/register_test_case.h"
+#include "framework/utility/ocl/profiling_helper.h"
+#include "framework/utility/timer.h"
+#include "multitile_memory_benchmark/definitions/usm_shared_migrate_gpu.h"
+
+#include <gtest/gtest.h>
+
+static TestResult run(const UsmSharedMigrateGpuArguments &arguments, Statistics &statistics) {
+    // Setup
+    const DeviceSelection queuePlacement = DeviceSelectionHelper::withoutHost(arguments.bufferPlacement);
+    QueueProperties queueProperties = QueueProperties::create().setDeviceSelection(queuePlacement).allowCreationFail();
+    ContextProperties contextProperties = ContextProperties::create().setDeviceSelection(arguments.contextPlacement).allowCreationFail();
+    Opencl opencl(queueProperties, contextProperties);
+    if (opencl.context == nullptr || opencl.commandQueue == nullptr) {
+        return TestResult::DeviceNotCapable;
+    }
+    auto clMemFreeINTEL = (pfn_clMemFreeINTEL)clGetExtensionFunctionAddressForPlatform(opencl.platform, "clMemFreeINTEL");
+    if (!UsmHelper::supportsUsm(opencl.platform)) {
+        return TestResult::DriverFunctionNotFound;
+    }
+
+    Timer timer;
+    cl_int retVal;
+
+    // Create buffer
+    cl_int *buffer = static_cast<cl_int *>(UsmHelper::allocate(arguments.bufferPlacement, opencl, arguments.bufferSize, &retVal));
+    ASSERT_CL_SUCCESS(retVal);
+    const size_t elementsCount = arguments.bufferSize / sizeof(cl_int);
+
+    // Create kernel
+    const char *source = "kernel void fill_with_ones(__global int *buffer) { const uint gid = get_global_id(0);  buffer[gid] = 1; }";
+    const auto sourceLength = strlen(source);
+    cl_program program = clCreateProgramWithSource(opencl.context, 1, &source, &sourceLength, &retVal);
+    ASSERT_CL_SUCCESS(retVal);
+    ASSERT_CL_SUCCESS(clBuildProgram(program, 0u, nullptr, nullptr, nullptr, nullptr));
+    cl_kernel kernel = clCreateKernel(program, "fill_with_ones", &retVal);
+    ASSERT_CL_SUCCESS(retVal);
+
+    // Warmup
+    for (auto elementIndex = 0u; elementIndex < elementsCount; elementIndex++) {
+        buffer[elementIndex] = 0;
+    }
+    const auto gws = elementsCount;
+    ASSERT_CL_SUCCESS(clSetKernelArgSVMPointer(kernel, 0, buffer));
+    ASSERT_CL_SUCCESS(clEnqueueNDRangeKernel(opencl.commandQueue, kernel, 1, nullptr, &gws, nullptr, 0, nullptr, nullptr));
+    ASSERT_CL_SUCCESS(clFinish(opencl.commandQueue));
+
+    // Benchmark
+    for (int i = 0; i < arguments.iterations; i++) {
+        // Migrate whole resource to CPU
+        for (auto elementIndex = 0u; elementIndex < elementsCount; elementIndex++) {
+            buffer[elementIndex] = 0;
+        }
+
+        // Measure kernel which must migrate the resource to GPU
+        timer.measureStart();
+        ASSERT_CL_SUCCESS(clEnqueueNDRangeKernel(opencl.commandQueue, kernel, 1, nullptr, &gws, nullptr, 0, nullptr, nullptr));
+        ASSERT_CL_SUCCESS(clFinish(opencl.commandQueue));
+        timer.measureEnd();
+
+        statistics.pushValue(timer.get(), arguments.bufferSize);
+    }
+
+    ASSERT_CL_SUCCESS(clReleaseKernel(kernel));
+    ASSERT_CL_SUCCESS(clReleaseProgram(program));
+    ASSERT_CL_SUCCESS(clMemFreeINTEL(opencl.context, buffer));
+    return TestResult::Success;
+}
+
+static RegisterTestCaseImplementation<UsmSharedMigrateGpu> registerTestCase(run, Api::OpenCL, true);
