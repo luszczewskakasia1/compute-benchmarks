f49187b584a5e27a1f688be800b6c130c61e6931
Maciej Dziuban <maciej.dziuban@intel.com>
2021-01-05 14:24:23 +0100

    Allow randomizing data - MapBuffer

--- source/memory_benchmark/gtest/map_buffer.cpp
+++ source/memory_benchmark/gtest/map_buffer.cpp
@@ -8,16 +8,17 @@
 
 static const inline RegisterTestCase<MapBuffer> registerTestCase{};
 
-class MapBufferTest : public ::testing::TestWithParam<std::tuple<Api, size_t, bool, MapFlags, bool>> {
+class MapBufferTest : public ::testing::TestWithParam<std::tuple<Api, size_t, BufferContents, bool, MapFlags, bool>> {
 };
 
 TEST_P(MapBufferTest, Test) {
     MapBufferArguments args;
     args.api = std::get<0>(GetParam());
     args.size = std::get<1>(GetParam());
-    args.compressed = std::get<2>(GetParam());
-    args.mapFlags = std::get<3>(GetParam());
-    args.useEvents = std::get<4>(GetParam());
+    args.contents = std::get<2>(GetParam());
+    args.compressed = std::get<3>(GetParam());
+    args.mapFlags = std::get<4>(GetParam());
+    args.useEvents = std::get<5>(GetParam());
 
     MapBuffer test;
     test.run(args);
@@ -30,6 +31,7 @@ INSTANTIATE_TEST_SUITE_P(
     ::testing::Combine(
         ::CommonGtestArgs::allApis(),
         ::testing::Values(128 * megaByte, 512 * megaByte),
+        ::testing::Values(BufferContents::Zeros),
         ::testing::Values(false, true),
         ::testing::Values(MapFlags::Read, MapFlags::Write, MapFlags::WriteInvalidate),
         ::testing::Values(false, true)));
--- source/memory_benchmark/implementations/ocl/map_buffer_ocl.cpp
+++ source/memory_benchmark/implementations/ocl/map_buffer_ocl.cpp
@@ -2,6 +2,7 @@
 #include "framework/ocl/map_flags_ocl.h"
 #include "framework/ocl/opencl.h"
 #include "framework/test_case/register_test_case.h"
+#include "framework/utility/ocl/buffer_contents_helper_ocl.h"
 #include "framework/utility/ocl/profiling_helper.h"
 #include "framework/utility/timer.h"
 #include "memory_benchmark/definitions/map_buffer.h"
@@ -31,11 +32,6 @@ static TestResult run(const MapBufferArguments &arguments, Statistics &statistic
         return compressionStatus;
     }
 
-    // Fill buffers
-    const char pattern[] = {0};
-    ASSERT_CL_SUCCESS(clEnqueueFillBuffer(opencl.commandQueue, buffer, pattern, sizeof(pattern) / sizeof(pattern[0]), 0, arguments.size, 0, nullptr, nullptr));
-    ASSERT_CL_SUCCESS(clFinish(opencl.commandQueue));
-
     // Warmup
     const auto mapFlags = convertMapFlags(arguments.mapFlags);
     void *ptr = clEnqueueMapBuffer(opencl.commandQueue, buffer, CL_BLOCKING, mapFlags, 0, arguments.size, 0, nullptr, nullptr, &retVal);
@@ -45,6 +41,8 @@ static TestResult run(const MapBufferArguments &arguments, Statistics &statistic
 
     // Benchmark
     for (int i = 0; i < arguments.iterations; i++) {
+        ASSERT_CL_SUCCESS(BufferContentsHelperOcl::fillBuffer(opencl.commandQueue, buffer, arguments.size, arguments.contents));
+
         cl_event profilingEvent{};
         cl_event *eventForEnqueue = arguments.useEvents ? &profilingEvent : nullptr;
 
