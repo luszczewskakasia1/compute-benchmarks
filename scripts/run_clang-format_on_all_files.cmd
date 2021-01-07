2035db2a71d4ab5102e30b471b683edf362e80eb
Maciej Dziuban <maciej.dziuban@intel.com>
2021-01-07 17:08:54 +0100

    Add scripts for reformatting

---
+++ scripts/run_clang-format_on_all_files.cmd
@@ -0,0 +1,25 @@
+@echo off
+setlocal enabledelayedexpansion
+
+SET CLANG_FORMAT_BINARY="clang-format.exe"
+SET EXTENSIONS_TO_PROCESS=(c, h, cpp, hpp, cxx, hxx, cc, inl)
+
+IF NOT EXIST %CLANG_FORMAT_BINARY% (
+  GOTO :FAIL
+)
+
+
+FOR %%e IN %EXTENSIONS_TO_PROCESS% DO (
+  ECHO Extension: %%e
+  FOR /R ..\source %%f IN (*.%%e) DO (
+    %CLANG_FORMAT_BINARY% -i -style=file %%f
+  )
+)
+
+:EXIT
+EXIT /B 0
+
+:FAIL
+echo.
+echo Script FAILED
+EXIT /B 1
