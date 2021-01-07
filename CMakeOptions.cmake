3d7799642a08270495fd57dc801417e7002f7be2
Maciej Dziuban <maciej.dziuban@intel.com>
2021-01-07 15:56:17 +0100

    Allow building without L0 or OCL, improve CMakeOptions

--- CMakeOptions.cmake
+++ CMakeOptions.cmake
@@ -1,18 +1,43 @@
-macro(benchmark_option NAME DEFAULT_VALUE)
+function(benchmark_option NAME DEFAULT_VALUE)
     if (NOT DEFINED ${NAME})
         set(${NAME} ${DEFAULT_VALUE})
     endif()
-    message(STATUS "  ${NAME}=${${NAME}}")
-endmacro()
-
+    message(STATUS "      ${NAME}=${${NAME}} ${ARGN}")
+endfunction()
+function (benchmark_option_group DESCRIPTION)
+    message(STATUS "   ${DESCRIPTION}:")
+endfunction()
 message(STATUS "Build options:")
-benchmark_option(OUTPUT_DIR "${CMAKE_BINARY_DIR}/bin")
-benchmark_option(BUILD_FOR_PUBLISHING OFF)
+
+# Selecting targets
+benchmark_option_group("Flags for selecting targets to be built")
+benchmark_option(BUILD_L0 ON)
+benchmark_option(BUILD_OCL ON)
+if (NOT BUILD_L0 OR NOT BUILD_OCL)
+    set(BUILD_ALL_API_BINARIES OFF)
+    set(BUILD_ALL_API_BINARIES_COMMENT "(Disabled due to not all APIs being enabled)")
+endif()
 benchmark_option(BUILD_SINGLE_API_BINARIES ON)
-benchmark_option(BUILD_ALL_API_BINARIES ON)
-benchmark_option(INCLUDE_VERSION OFF)
+benchmark_option(BUILD_ALL_API_BINARIES ON ${BUILD_ALL_API_BINARIES_COMMENT})
+
+# Publish builds and versioning
+benchmark_option_group("Flags for configuring publishable builds and versions")
+benchmark_option(BUILD_FOR_PUBLISHING OFF)
+if (BUILD_FOR_PUBLISHING)
+    set(INCLUDE_VERSION_COMMENT "(Enabled by BUILD_FOR_PUBLISHING)")
+endif()
+benchmark_option(INCLUDE_VERSION OFF ${INCLUDE_VERSION_COMMENT})
+
+# Miscellaneous flags
+benchmark_option_group("Miscellaneous flags")
+benchmark_option(OUTPUT_DIR "${CMAKE_BINARY_DIR}/bin")
 if (UNIX)
     benchmark_option(USE_PCIACCESS ON)
 else()
     set(USE_PCIACCESS OFF)
+endif()
+
+# Additional checks
+if (NOT BUILD_L0 AND NOT BUILD_OCL)
+    message(FATAL_ERROR "No API was selected for testing. No benchmarks will be produced")
 endif()
