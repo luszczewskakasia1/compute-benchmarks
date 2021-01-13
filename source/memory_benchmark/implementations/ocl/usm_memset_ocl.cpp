2e1aa1dc4f57fa9a02ca14b2f9787dfdd5a6cd41
Michal Mrozek <michal.mrozek@intel.com>
2021-01-13 12:40:42 +0100

    Fix queue creation.

--- source/memory_benchmark/implementations/ocl/usm_memset_ocl.cpp
+++ source/memory_benchmark/implementations/ocl/usm_memset_ocl.cpp
@@ -11,7 +11,7 @@
 
 static TestResult run(const UsmMemsetArguments &arguments, Statistics &statistics) {
     // Setup
-    QueueProperties queueProperties = QueueProperties::create().setForceBlitter(arguments.forceBlitter).allowCreationFail();
+    QueueProperties queueProperties = QueueProperties::create().setForceBlitter(arguments.forceBlitter).setProfiling(arguments.useEvents).allowCreationFail();
     Opencl opencl(queueProperties);
     if (opencl.commandQueue == nullptr) {
         return TestResult::DeviceNotCapable;
