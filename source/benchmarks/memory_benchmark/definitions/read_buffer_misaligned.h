b8211a457b7f4fe89ffb863412e30bbb7c41eab2
Maciej Dziuban <maciej.dziuban@intel.com>
2021-01-22 14:20:10 +0100

    Add ReadBufferMisaligned benchmark

---
+++ source/benchmarks/memory_benchmark/definitions/read_buffer_misaligned.h
@@ -0,0 +1,31 @@
+#pragma once
+
+#include "framework/test_case/test_case.h"
+#include "framework/test_case_argument/enum/test_case_argument_buffer_contents.h"
+#include "framework/test_case_argument/test_case_argument_compression.h"
+#include "framework/utility/common_help_message.h"
+
+struct ReadBufferMisalignedArguments : TestCaseArguments {
+    ByteSizeTestCaseArgument size;
+    NonNegativeIntegerTestCaseArgument misalignmentFromCacheline;
+    BooleanTestCaseArgument useEvents;
+
+    ReadBufferMisalignedArguments()
+        : size(*this, "size", "Size of the buffer"),
+          misalignmentFromCacheline(*this, "misalignment", "Number of bytes by which misaligned the destination pointer will be misaligned"),
+          useEvents(*this, "useEvents", CommonHelpMessage::useEvents()) {}
+};
+
+struct ReadBufferMisaligned : TestCase<ReadBufferMisalignedArguments> {
+    using TestCase<ReadBufferMisalignedArguments>::TestCase;
+
+    std::string getTestCaseName() const override {
+        return "ReadBufferMisaligned";
+    }
+
+    std::string getHelp() const override {
+        return "allocates an OpenCL buffer and measures read bandwidth. Read operation means "
+               "transfer from GPU to CPU. Destination pointer passed by the application will be "
+               "misaligned by the specified amount of bytes.";
+    }
+};
---
+++ source/benchmarks/memory_benchmark/implementations/ocl/read_buffer_misaligned_ocl.cpp
@@ -0,0 +1,53 @@
+#include "framework/ocl/buffer_contents_helper_ocl.h"
+#include "framework/ocl/compression_helper.h"
+#include "framework/ocl/opencl.h"
+#include "framework/ocl/profiling_helper.h"
+#include "framework/test_case/register_test_case.h"
+#include "framework/utility/cpu_allocation_helper.h"
+#include "framework/utility/memory_constants.h"
+#include "framework/utility/timer.h"
+
+#include "definitions/read_buffer_misaligned.h"
+
+#include <gtest/gtest.h>
+
+static TestResult run(const ReadBufferMisalignedArguments &arguments, Statistics &statistics) {
+    // Setup
+    QueueProperties queueProperties = QueueProperties::create().setProfiling(arguments.useEvents);
+    Opencl opencl(queueProperties);
+    Timer timer;
+    cl_int retVal;
+
+    // Create buffer
+    const cl_mem buffer = clCreateBuffer(opencl.context, CL_MEM_READ_WRITE, arguments.size, nullptr, &retVal);
+    ASSERT_CL_SUCCESS(retVal);
+    auto cpuBuffer = CpuAllocationHelper::allocateMisalignedAllocation(arguments.size, MemoryConstants::cachelineSize, arguments.misalignmentFromCacheline);
+
+    // Warmup
+    ASSERT_CL_SUCCESS(clEnqueueReadBuffer(opencl.commandQueue, buffer, CL_BLOCKING, 0, arguments.size, cpuBuffer.get(), 0, nullptr, nullptr));
+
+    // Benchmark
+    for (int i = 0; i < arguments.iterations; i++) {
+        cl_event profilingEvent{};
+        cl_event *eventForEnqueue = arguments.useEvents ? &profilingEvent : nullptr;
+
+        timer.measureStart();
+        ASSERT_CL_SUCCESS(clEnqueueReadBuffer(opencl.commandQueue, buffer, CL_NON_BLOCKING, 0, arguments.size, cpuBuffer.get(), 0, nullptr, eventForEnqueue));
+        ASSERT_CL_SUCCESS(clFinish(opencl.commandQueue))
+        timer.measureEnd();
+
+        if (eventForEnqueue) {
+            cl_ulong timeNs{};
+            ASSERT_CL_SUCCESS(ProfilingHelper::getEventDurationInNanoseconds(profilingEvent, timeNs));
+            ASSERT_CL_SUCCESS(clReleaseEvent(profilingEvent));
+            statistics.pushValue(std::chrono::nanoseconds(timeNs), arguments.size);
+        } else {
+            statistics.pushValue(timer.get(), arguments.size);
+        }
+    }
+
+    ASSERT_CL_SUCCESS(clReleaseMemObject(buffer));
+    return TestResult::Success;
+}
+
+static RegisterTestCaseImplementation<ReadBufferMisaligned> registerTestCase(run, Api::OpenCL);
