d195232f1998ca29dd01622be25d5840cc42d518
Maciej Dziuban <maciej.dziuban@intel.com>
2021-02-08 13:22:12 +0100

    Create HexHelper

--- source/framework/test_case_argument/test_case_argument_long_hex.h
+++ source/framework/test_case_argument/test_case_argument_long_hex.h
@@ -1,10 +1,7 @@
 #pragma once
 
-#include "framework/configuration.h"
 #include "framework/test_case_argument/test_case_argument_basic.h"
-
-#include <iomanip>
-#include <vector>
+#include "framework/utility/hex_helper.h"
 
 struct LongHexTestCaseArgument : TestCaseArgument {
     using TestCaseArgument::TestCaseArgument;
@@ -24,39 +21,11 @@ struct LongHexTestCaseArgument : TestCaseArgument {
 
   protected:
     std::string toStringValue() const override {
-        std::ostringstream output{};
-        output << "0x";
-        for (auto byte : bytes) {
-            output << std::uppercase << std::hex                      // print uppercase hex letters
-                   << std::setfill('0') << std::setw(2) << std::right // always print 2 signs: 0x01 instead of 0x1
-                   << static_cast<uint32_t>(byte);                    // cast to bigger type so it's not treated as char
-        }
-        return output.str();
+        return HexHelper::toHex(bytes);
     }
 
     void parseImpl(const std::string &value) override {
-        bytes.resize(0);
-
-        // Process prefix
-        const std::string prefix = "0x";
-        if (value.find(prefix) != 0) {
-            return;
-        }
-
-        // Each byte is two characters, so length should be even
-        if (value.length() % 2 != 0) {
-            return;
-        }
-
-        // Iterate over bytes
-        const auto bytesCount = value.length() / 2 - 1;
-        bytes.reserve(bytesCount);
-        for (auto byteIndex = 0u; byteIndex < bytesCount; byteIndex++) {
-            const auto byteStringOffset = 2 + 2 * byteIndex;
-            const auto byteString = value.substr(byteStringOffset, 2);
-            const auto byte = static_cast<uint8_t>(std::stoul(byteString, nullptr, 16));
-            bytes.push_back(byte);
-        }
+        bytes = HexHelper::fromHex(value);
     }
 
     std::vector<uint8_t> bytes;
