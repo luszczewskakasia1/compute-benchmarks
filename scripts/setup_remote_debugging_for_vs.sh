d3cc664c368ef0fc993511bc0aec599f79bebf8c
Maciej Dziuban <maciej.dziuban@intel.com>
2021-03-12 17:41:09 +0100

    Add script for setting up VS remote debugger

---
+++ scripts/setup_remote_debugging_for_vs.sh
@@ -0,0 +1,22 @@
+echo -n "Machine IP: "
+read ip
+
+while IFS= read -r settings_file; do
+    benchmark_name=`basename $settings_file | cut -d'.' -f1`
+
+    echo "Setting up $benchmark_name..."
+
+    echo "<?xml version=\"1.0\" encoding=\"utf-8\"?>                                                       "   > $settings_file
+    echo "<Project ToolsVersion=\"Current\" xmlns=\"http://schemas.microsoft.com/developer/msbuild/2003\"> "  >> $settings_file
+    echo "  <PropertyGroup Condition=\"'\$(Configuration)|\$(Platform)'=='Debug|x64'\">                    "  >> $settings_file
+    echo "    <RemoteDebuggerCommand>S:\\a\\Debug\\$benchmark_name.exe</RemoteDebuggerCommand>             "  >> $settings_file
+    echo "    <RemoteDebuggerCommandArguments></RemoteDebuggerCommandArguments>                            "  >> $settings_file
+    echo "    <RemoteDebuggerWorkingDirectory>S:\\a\\Debug</RemoteDebuggerWorkingDirectory>                "  >> $settings_file
+    echo "    <RemoteDebuggerServerName>$ip:4020</RemoteDebuggerServerName>                                "  >> $settings_file
+    echo "    <RemoteDebuggerConnection>RemoteWithoutAuthentication</RemoteDebuggerConnection>             "  >> $settings_file
+    echo "    <DebuggerFlavor>WindowsRemoteDebugger</DebuggerFlavor>                                       "  >> $settings_file
+    echo "  </PropertyGroup>                                                                               "  >> $settings_file
+    echo "</Project>                                                                                       "  >> $settings_file
+done < <(find ../build/source/ -name "*.vcxproj.user" | grep benchmarks/.*_benchmark)
+
+echo "Done!"
