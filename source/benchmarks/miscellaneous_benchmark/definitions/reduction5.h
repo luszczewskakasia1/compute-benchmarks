355cc28b28e2896daf1060917f085cc77243e2ab
Michal Mrozek <michal.mrozek@intel.com>
2021-03-24 12:43:25 +0100

    Reduction 5

---
+++ source/benchmarks/miscellaneous_benchmark/definitions/reduction5.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include "framework/test_case/test_case.h"
+
+struct ReductionArguments5 : TestCaseArgumentContainer {
+    PositiveIntegerArgument numberOfElements;
+
+    ReductionArguments5()
+        : numberOfElements(*this, "numberOfElements", "Number of elements that will be reduced") {}
+};
+
+struct Reduction5 : TestCase<ReductionArguments5> {
+    using TestCase<ReductionArguments5>::TestCase;
+
+    std::string getTestCaseName() const override {
+        return "Reduction";
+    }
+
+    std::string getHelp() const override {
+        return "Performs a reduction operation on a buffer. Each thread performs atomic_add on "
+               "one shared memory location.";
+    }
+};
