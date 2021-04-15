2be98508fdf1be47ebec9d5e35d868e1dec149f6
Maciej Dziuban <maciej.dziuban@intel.com>
2021-04-15 17:58:07 +0200

    Give bigger portions to transfer for BCS0

--- source/benchmarks/memory_benchmark/implementations/ocl/usm_copy_multiple_blits_ocl.cpp
+++ source/benchmarks/memory_benchmark/implementations/ocl/usm_copy_multiple_blits_ocl.cpp
@@ -22,14 +22,25 @@ static TestResult run(const UsmCopyMultipleBlitsArguments &arguments, Statistics
 
     // Create selected blitter queues
     struct PerQueueData {
-        cl_command_queue queue = nullptr; // freed by the OpenCL class
-        std::string name = "";
-        void *srcBuffer = nullptr;
-        void *dstBuffer = nullptr;
+        cl_command_queue queue; // freed by the OpenCL class
+        std::string name;
+        bool isLinkCopyEngine;
         cl_event event = nullptr;
+        void *copySrc;
+        void *copyDst;
+        size_t copySize = 0;
     };
+    size_t mainCopyEngineCount = 0;
+    size_t linkCopyEngineCount = 0;
     std::vector<PerQueueData> queues;
     for (size_t blitterIndex : arguments.blitters.getEnabledBits()) {
+        const bool isLinkCopyEngine = blitterIndex > 0;
+        if (isLinkCopyEngine) {
+            linkCopyEngineCount++;
+        } else {
+            mainCopyEngineCount++;
+        }
+
         const Engine engine = EngineHelper::getBlitterEngineFromIndex(blitterIndex);
         const QueueProperties blitterQueueProperties = QueueProperties::create().setForceEngine(engine).setProfiling(true);
         const cl_command_queue queue = opencl.createQueue(blitterQueueProperties);
@@ -37,20 +48,35 @@ static TestResult run(const UsmCopyMultipleBlitsArguments &arguments, Statistics
             return TestResult::DeviceNotCapable;
         }
         const std::string queueName = EngineHelper::getEngineName(engine);
-        queues.push_back(PerQueueData{queue, queueName});
+        queues.push_back(PerQueueData{queue, queueName, isLinkCopyEngine});
     }
 
     // Create buffers
-    for (PerQueueData &queue : queues) {
-        queue.srcBuffer = UsmHelper::allocate(arguments.sourcePlacement, opencl.platform, opencl.context, opencl.device, arguments.size, &retVal);
-        ASSERT_CL_SUCCESS(retVal);
-        queue.dstBuffer = UsmHelper::allocate(arguments.destinationPlacement, opencl.platform, opencl.context, opencl.device, arguments.size, &retVal);
-        ASSERT_CL_SUCCESS(retVal);
+    void *srcBuffer = UsmHelper::allocate(arguments.sourcePlacement, opencl.platform, opencl.context, opencl.device, arguments.size, &retVal);
+    ASSERT_CL_SUCCESS(retVal);
+    void *dstBuffer = UsmHelper::allocate(arguments.destinationPlacement, opencl.platform, opencl.context, opencl.device, arguments.size, &retVal);
+    ASSERT_CL_SUCCESS(retVal);
+
+    // Calculate copyOffset and copySize for each copy engine
+    const size_t chunksCount = linkCopyEngineCount + mainCopyEngineCount * linkCopyEngineCount;
+    const size_t chunksSize = arguments.size / chunksCount;
+    const size_t copySizeForMainCopyEngine = chunksSize * linkCopyEngineCount;
+    const size_t copySizeForLinkCopyEngine = chunksSize;
+    size_t copyOffset = 0;
+    for (auto i = 0u; i < queues.size(); i++) {
+        const size_t copySize = queues[i].isLinkCopyEngine ? copySizeForLinkCopyEngine : copySizeForMainCopyEngine;
+        queues[i].copySrc = static_cast<char *>(srcBuffer) + copyOffset;
+        queues[i].copyDst = static_cast<char *>(dstBuffer) + copyOffset;
+        queues[i].copySize = copySize;
+        copyOffset += copySize;
     }
+    DEVELOPER_WARNING_IF(copyOffset < arguments.size, "Buffer is divided to ", chunksCount, " chunks, but bufferSize is ",
+                         "not divisible by ", chunksCount, ". Only ", copyOffset, " out of specified ", arguments.size, " bytes will be copied.");
+    DEVELOPER_WARNING_IF(copyOffset > arguments.size, "Access out of buffer bounds will happen");
 
     // Warmup
     for (PerQueueData &queue : queues) {
-        ASSERT_CL_SUCCESS(clEnqueueMemcpyINTEL(queue.queue, CL_FALSE, queue.dstBuffer, queue.srcBuffer, arguments.size, 0, nullptr, nullptr));
+        ASSERT_CL_SUCCESS(clEnqueueMemcpyINTEL(queue.queue, CL_FALSE, queue.copyDst, queue.copySrc, queue.copySize, 0, nullptr, nullptr));
         ASSERT_CL_SUCCESS(clFlush(queue.queue));
     }
     for (PerQueueData &queue : queues) {
@@ -61,7 +87,7 @@ static TestResult run(const UsmCopyMultipleBlitsArguments &arguments, Statistics
     for (int i = 0; i < arguments.iterations; i++) {
         timer.measureStart();
         for (PerQueueData &queue : queues) {
-            ASSERT_CL_SUCCESS(clEnqueueMemcpyINTEL(queue.queue, CL_FALSE, queue.dstBuffer, queue.srcBuffer, arguments.size, 0, nullptr, &queue.event));
+            ASSERT_CL_SUCCESS(clEnqueueMemcpyINTEL(queue.queue, CL_FALSE, queue.copyDst, queue.copySrc, queue.copySize, 0, nullptr, &queue.event));
         }
         for (PerQueueData &queue : queues) {
             ASSERT_CL_SUCCESS(clFlush(queue.queue));
@@ -77,21 +103,18 @@ static TestResult run(const UsmCopyMultipleBlitsArguments &arguments, Statistics
             cl_ulong timeNs = 0ul;
             ASSERT_CL_SUCCESS(ProfilingHelper::getEventDurationInNanoseconds(queue.event, timeNs));
             ASSERT_CL_SUCCESS(clReleaseEvent(queue.event));
-            statistics.pushValue(std::chrono::nanoseconds(timeNs), arguments.size, queue.name);
+            statistics.pushValue(std::chrono::nanoseconds(timeNs), queue.copySize, queue.name);
 
             maxGpuTime = std::max(maxGpuTime, std::chrono::nanoseconds(timeNs));
         }
 
         // Report total results
-        const uint64_t totalSize = arguments.size * queues.size();
-        statistics.pushValue(maxGpuTime, totalSize, "Total (Gpu)");
-        statistics.pushValue(timer.get(), totalSize, "Total (Cpu)");
+        statistics.pushValue(maxGpuTime, arguments.size, "Total (Gpu)");
+        statistics.pushValue(timer.get(), arguments.size, "Total (Cpu)");
     }
 
-    for (PerQueueData &queue : queues) {
-        ASSERT_CL_SUCCESS(clMemFreeINTEL(opencl.context, queue.srcBuffer));
-        ASSERT_CL_SUCCESS(clMemFreeINTEL(opencl.context, queue.dstBuffer));
-    }
+    ASSERT_CL_SUCCESS(clMemFreeINTEL(opencl.context, srcBuffer));
+    ASSERT_CL_SUCCESS(clMemFreeINTEL(opencl.context, dstBuffer));
     return TestResult::Success;
 }
 
