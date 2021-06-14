c70bc09672bc67ed793bbf8a25c42eb152f5e8f8
Maciej Dziuban <maciej.dziuban@intel.com>
2021-06-14 11:16:53 +0200

    Order tests lexicographically

--- source/framework/test_map.h
+++ source/framework/test_map.h
@@ -6,7 +6,13 @@
 #include <memory>
 #include <string>
 
-struct TestMap : std::map<std::string, std::unique_ptr<TestCaseInterface>> {
+struct LexicographicalLess {
+    bool operator()(const std::string &left, const std::string &right) const {
+        return left < right;
+    }
+};
+
+struct TestMap : std::map<std::string, std::unique_ptr<TestCaseInterface>, LexicographicalLess> {
     static TestMap &get() {
         static TestMap testMap = {};
         return testMap;
