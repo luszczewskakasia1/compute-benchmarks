fad3268acde2aa2db255e3e47abf6c3ebbbd0b5d
Maciej Dziuban <maciej.dziuban@intel.com>
2021-07-13 13:19:33 +0200

    Move main.cpp code into the framework

---
+++ source/framework/benchmark_main.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#include "framework/utility/command_line_argument.h"
+
+#include <string>
+
+class BenchmarkMain {
+  public:
+    BenchmarkMain(int argc, char **argv, const std::string benchmarkVersion);
+    int main();
+
+  private:
+    int argc;
+    char **argv;
+    const std::string benchmarkVersion;
+    CommandLineArguments commandLineArguments = {};
+
+    int setupEnvironment();
+
+    int printVersion(bool enableWarning, const char *prefix = "");
+    void printHelp();
+    void generateDocs(const std::string &docsFileName);
+
+    int executeSingleTest(const std::string &testName, CommandLineArguments &commandLineArguments);
+    int executeAllTests(CommandLineArguments &commandLineArguments);
+};
