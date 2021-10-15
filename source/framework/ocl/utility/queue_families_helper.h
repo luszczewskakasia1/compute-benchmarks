7839afa63b4b80e5bc67ac0548b9a706cf98cd52
Maciej Dziuban <maciej.dziuban@intel.com>
2021-10-15 13:03:22 +0200

    Format code
    Signed-off-by: Maciej Dziuban <maciej.dziuban@intel.com>

--- source/framework/ocl/utility/queue_families_helper.h
+++ source/framework/ocl/utility/queue_families_helper.h
@@ -98,7 +98,7 @@ class QueueFamiliesHelper {
     }
 
     template <typename... Args>
-    static bool validateCapabilities(cl_command_queue queue, Args &&...args) {
+    static bool validateCapabilities(cl_command_queue queue, Args &&... args) {
         return validateCapabilities(getQueueCapabilities(queue), std::forward<Args>(args)...);
     }
 
@@ -161,7 +161,7 @@ class QueueFamiliesHelper {
     }
 
     template <typename... Args>
-    static bool validateCapabilities(cl_command_queue_capabilities_intel queueCapabilities, cl_command_queue_capabilities_intel capability, Args &&...args) {
+    static bool validateCapabilities(cl_command_queue_capabilities_intel queueCapabilities, cl_command_queue_capabilities_intel capability, Args &&... args) {
         if (!validateCapability(queueCapabilities, capability)) {
             return false;
         }
