de63067f45288f259552b5e4558a28f8161b7eeb
Maciej Dziuban <maciej.dziuban@intel.com>
2021-10-15 11:30:06 +0200

    Fix warnings 1/n - cast pointer from void* before deleting
    Signed-off-by: Maciej Dziuban <maciej.dziuban@intel.com>

--- source/framework/ocl/utility/hostptr_reuse_helper.cpp
+++ source/framework/ocl/utility/hostptr_reuse_helper.cpp
@@ -54,7 +54,7 @@ cl_int HostptrReuseHelper::allocateBufferHostptr(Opencl &opencl,
 cl_int HostptrReuseHelper::deallocateBufferHostptr(Alloc alloc) {
     switch (alloc.reuseMode) {
     case HostptrReuseMode::None:
-        delete[](alloc.ptr);
+        delete[](static_cast<uint8_t *>(alloc.ptr));
         break;
     case HostptrReuseMode::Usm:
         CL_SUCCESS_OR_RETURN(alloc.clMemFreeINTEL(alloc.context, alloc.ptr));
