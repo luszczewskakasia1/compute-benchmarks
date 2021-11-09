a4bb88c71ecadc615f83701198869b46b0054a3f
LukaszJobczyk <lukasz.jobczyk@intel.com>
2021-11-09 07:17:00 +0100

    Add multi queue submission benchmark (#61)
    Signed-off-by: Lukasz Jobczyk <lukasz.jobczyk@intel.com>

---
+++ source/benchmarks/ulls_benchmark/definitions/multi_queue_submission.h
@@ -0,0 +1,42 @@
+/*
+ * INTEL CONFIDENTIAL
+ *
+ * Copyright (C) 2021 Intel Corporation
+ *
+ * This software and the related documents are Intel copyrighted materials,
+ * and your use of them is governed by the express license under which they were
+ * provided to you ("License"). Unless the License provides otherwise,
+ * you may not use, modify, copy, publish, distribute, disclose or transmit this
+ * software or the related documents without Intel's prior written permission.
+ *
+ * This software and the related documents are provided as is, with no express or
+ * implied warranties, other than those that are expressly stated in the License.
+ */
+
+#pragma once
+
+#include "framework/argument/basic_argument.h"
+#include "framework/test_case/test_case.h"
+
+struct MultiQueueSubmissionArguments : TestCaseArgumentContainer {
+    PositiveIntegerArgument queueCount;
+    PositiveIntegerArgument workgroupCount;
+    PositiveIntegerArgument workgroupSize;
+
+    MultiQueueSubmissionArguments()
+        : queueCount(*this, "queueCount", "Number of command queues created"),
+          workgroupCount(*this, "wgc", "Workgroup count"),
+          workgroupSize(*this, "wgs", "Workgroup size") {}
+};
+
+struct MultiQueueSubmission : TestCase<MultiQueueSubmissionArguments> {
+    using TestCase<MultiQueueSubmissionArguments>::TestCase;
+
+    std::string getTestCaseName() const override {
+        return "MultiQueueSubmission";
+    }
+
+    std::string getHelp() const override {
+        return "enqueues kernel on multiple command queues";
+    }
+};
---
+++ source/benchmarks/ulls_benchmark/gtest/multi_queue_submission.cpp
@@ -0,0 +1,45 @@
+/*
+ * INTEL CONFIDENTIAL
+ *
+ * Copyright (C) 2021 Intel Corporation
+ *
+ * This software and the related documents are Intel copyrighted materials,
+ * and your use of them is governed by the express license under which they were
+ * provided to you ("License"). Unless the License provides otherwise,
+ * you may not use, modify, copy, publish, distribute, disclose or transmit this
+ * software or the related documents without Intel's prior written permission.
+ *
+ * This software and the related documents are provided as is, with no express or
+ * implied warranties, other than those that are expressly stated in the License.
+ */
+
+#include "definitions/multi_queue_submission.h"
+
+#include "framework/test_case/register_test_case.h"
+#include "framework/utility/common_gtest_args.h"
+
+#include <gtest/gtest.h>
+
+static const inline RegisterTestCase<MultiQueueSubmission> registerTestCase{};
+
+class MultiQueueSubmissionTest : public ::testing::TestWithParam<std::tuple<size_t, size_t, size_t>> {
+};
+
+TEST_P(MultiQueueSubmissionTest, Test) {
+    MultiQueueSubmissionArguments args{};
+    args.api = Api::OpenCL;
+    args.queueCount = std::get<0>(GetParam());
+    args.workgroupCount = std::get<1>(GetParam());
+    args.workgroupSize = std::get<2>(GetParam());
+
+    MultiQueueSubmission test;
+    test.run(args);
+}
+
+INSTANTIATE_TEST_SUITE_P(
+    MultiQueueSubmissionTest,
+    MultiQueueSubmissionTest,
+    ::testing::Combine(
+        ::testing::Values(2, 4, 8, 16, 32, 64, 128),
+        CommonGtestArgs::workgroupCount(),
+        CommonGtestArgs::workgroupSize()));
