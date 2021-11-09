e637a004d98bdd26f20ce5cf21044223435530d9
Maciej Dziuban <maciej.dziuban@intel.com>
2021-11-09 11:09:33 +0100

    Use ValuesIn

--- source/benchmarks/memory_benchmark/gtest/usm_copy.cpp
+++ source/benchmarks/memory_benchmark/gtest/usm_copy.cpp
@@ -46,8 +46,8 @@ INSTANTIATE_TEST_SUITE_P(
     UsmCopyTest,
     ::testing::Combine(
         ::CommonGtestArgs::allApis(),
-        ::testing::Values(UsmMemoryPlacement::Device, UsmMemoryPlacement::Host, UsmMemoryPlacement::Shared, UsmMemoryPlacement::NonUsm, UsmMemoryPlacement::NonUsmImported),
-        ::testing::Values(UsmMemoryPlacement::Device, UsmMemoryPlacement::Host, UsmMemoryPlacement::Shared, UsmMemoryPlacement::NonUsm, UsmMemoryPlacement::NonUsmImported),
+        ::testing::ValuesIn(UsmMemoryPlacementArgument::enumValues),
+        ::testing::ValuesIn(UsmMemoryPlacementArgument::enumValues),
         ::testing::Values(128 * megaByte, 512 * megaByte),
         ::testing::Values(BufferContents::Zeros),
         ::testing::Values(false, true),
