57c6e1258e070574b07c9dc071ab01dc99a5b160
Young Jin Yoon <young.jin.yoon@intel.com>
2021-12-09 16:10:26 -0800

    Add KernelWithEvent test
    Related-To: LOCI-2788
    Signed-off-by: Young Jin Yoon <young.jin.yoon@intel.com>

--- source/benchmarks/gpu_cmds_benchmark/benchmark_info.cpp
+++ source/benchmarks/gpu_cmds_benchmark/benchmark_info.cpp
@@ -20,6 +20,6 @@
 EXECUTE_AT_APP_INIT {
     const std::string name = "gpu_cmds_benchmark";
     const std::string description = "Gpu Commands Benchmark is a set of tests aimed at measuring GPU-side execution duration of various commands.";
-    const int testCaseColumnWidth = 90;
+    const int testCaseColumnWidth = 120;
     BenchmarkInfo::initialize(name, description, testCaseColumnWidth);
 };
