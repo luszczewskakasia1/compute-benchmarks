b3cf66ba387d814e219fc304b9b905fb240194ad
Dominik Dabek <dominik.dabek@intel.com>
2022-02-22 11:21:43 +0100

    Add l0, create/destroy immediate cmdlist bench
    Related-To: NEO-6707
    
    Signed-off-by: Dominik Dabek <dominik.dabek@intel.com>

---
+++ source/benchmarks/api_overhead_benchmark/implementations/l0/destroy_command_list_immediate_l0.cpp
@@ -0,0 +1,62 @@
+/*
+ * INTEL CONFIDENTIAL
+ *
+ * Copyright (C) 2022 Intel Corporation
+ *
+ * This software and the related documents are Intel copyrighted materials,
+ * and your use of them is governed by the express license under which they were
+ * provided to you ("License"). Unless the License provides otherwise,
+ * you may not use, modify, copy, publish, distribute, disclose or transmit this
+ * software or the related documents without Intel's prior written permission.
+ *
+ * This software and the related documents are provided as is, with no express or
+ * implied warranties, other than those that are expressly stated in the License.
+ */
+
+#include "framework/l0/levelzero.h"
+#include "framework/test_case/register_test_case.h"
+#include "framework/utility/timer.h"
+
+#include "definitions/destroy_command_list_immediate.h"
+
+#include <gtest/gtest.h>
+
+static TestResult run(const DestroyCommandListImmediateArguments &arguments, Statistics &statistics) {
+    // Setup
+    QueueProperties queueProperties = QueueProperties::create().disable();
+    LevelZero levelzero(queueProperties);
+    Timer timer;
+
+    // Warmup
+    std::vector<ze_command_list_handle_t> commandLists(arguments.cmdListCount);
+    ze_command_queue_desc_t commandQueueDesc = {ZE_STRUCTURE_TYPE_COMMAND_QUEUE_DESC};
+    ze_command_list_handle_t commandList;
+    for (auto i = 0u; i < arguments.cmdListCount; i++) {
+        ASSERT_ZE_RESULT_SUCCESS(zeCommandListCreateImmediate(levelzero.context, levelzero.device, &commandQueueDesc, &commandList));
+        EXPECT_NE(nullptr, commandList);
+        commandLists[i] = commandList;
+    }
+    for (auto i = 0u; i < arguments.cmdListCount; i++) {
+        ASSERT_ZE_RESULT_SUCCESS(zeCommandListDestroy(commandLists[i]));
+        commandLists[i] = nullptr;
+    }
+
+    // Benchmark
+    for (auto j = 0u; j < arguments.iterations; j++) {
+        for (auto i = 0u; i < arguments.cmdListCount; i++) {
+            ASSERT_ZE_RESULT_SUCCESS(zeCommandListCreateImmediate(levelzero.context, levelzero.device, &commandQueueDesc, &commandList));
+            commandLists[i] = commandList;
+        }
+
+        timer.measureStart();
+        for (auto i = 0u; i < arguments.cmdListCount; i++) {
+            ASSERT_ZE_RESULT_SUCCESS(zeCommandListDestroy(commandLists[i]));
+        }
+        timer.measureEnd();
+
+        statistics.pushValue(timer.get(), MeasurementUnit::Microseconds, MeasurementType::Cpu);
+    }
+    return TestResult::Success;
+}
+
+static RegisterTestCaseImplementation<DestroyCommandListImmediate> registerTestCase(run, Api::L0);
